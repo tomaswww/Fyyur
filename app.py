69eed3c4b7557add2a73a6646b5971de23cca89e
tomaswww <twingord@gmail.com>
2019-09-29 03:17:22 -0300

    Added edit venue initial rendering functionality

--- app.py
+++ app.py
@@ -409,8 +409,7 @@ def show_artist(artist_id):
 def edit_artist(artist_id):
   form = ArtistForm()
   artist = Artist.query.filter_by(id=artist_id).first()
-  
-  # TODO: populate form with fields from artist with ID <artist_id> --> DONE (Name only here, for placeholder population see edit_artist.html file)
+  # TODO: populate form with fields from artist with ID <artist_id> --> DONE (Name only here, for rest population see edit_artist.html file)
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
@@ -423,21 +422,8 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
   form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  # TODO: populate form with values from venue with ID <venue_id>
+  venue = Venue.query.filter_by(id=venue_id).first()
+  # TODO: populate form with values from venue with ID <venue_id> --> DONE (Name only here, for rest population see edit_venue.html file)
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
