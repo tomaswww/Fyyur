16396670213af237eefb8700ad3acee5b4714d94
tomaswww <twingord@gmail.com>
2019-09-30 00:41:55 -0300

    Added functionality for rendering shows

--- app.py
+++ app.py
@@ -115,8 +115,6 @@ def index():
 
 @app.route('/venues')
 def venues():
-  # TODO: replace with real venues data.
-  #       num_shows should be aggregated based on number of upcoming shows per venue.
   # Initialize variables
   data = []
   venue_info = {}
@@ -146,6 +144,9 @@ def venues():
   data.append(venue_info)
 
   return render_template('pages/venues.html', areas=data)
+
+  # TODO: replace with real venues data.
+  #       num_shows should be aggregated based on number of upcoming shows per venue. --> ALL DONE HERE
 
 #HERE I HAVE TO FIND A WAY TO ORDER DATA TO BE RETURNED AS:
   
@@ -377,7 +378,7 @@ def search_artists():
 
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
+  # search for "band" should return "The Wild Sax Band". --> ALL DONE HERE
   
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
@@ -535,45 +536,23 @@ def create_artist_submission():
 
 @app.route('/shows')
 def shows():
+  # Initialize variables
+  data =[]
   # displays list of shows at /shows
-  # TODO: replace with real venues data.
-  #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "venue_id": 1,
-    "venue_name": "The Musical Hop",
-    "artist_id": 4,
-    "artist_name": "Guns N Petals",
-    "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-    "start_time": "2019-05-21T21:30:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 5,
-    "artist_name": "Matt Quevedo",
-    "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    "start_time": "2019-06-15T23:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-01T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-08T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-15T20:00:00.000Z"
-  }]
+  # TODO: replace with real shows data.
+  results = db.session.query(Show.venue_id,Venue.name,Show.artist_id,Artist.name,Artist.image_link,Show.start_time).all()
+  for result in results:
+    show_info = {}
+    show_info["venue_id"] = result[0]
+    show_info["venue_name"] = result[1]
+    show_info["artist_id"] = result[2]
+    show_info["artist_name"] = result[3]
+    show_info["artist_image_link"]= result[4]
+    new_date_format = str(result[5])
+    new_date = format_datetime(new_date_format, format='medium')
+    show_info["start_time"]= new_date
+    data.append(show_info)
+  #print(data)
   return render_template('pages/shows.html', shows=data)
 
 @app.route('/shows/create')
@@ -603,7 +582,7 @@ def create_show_submission():
     db.session.close()
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/ --> DONE
   return render_template('pages/home.html')
 
 @app.errorhandler(404)
